2d82d8a92e277798421721522ed3ae92018dba55
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-04-09 22:36:51 -0300

    Add test directory in CR project

---
+++ cr/test/test_io.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ronaldd on 09/04/2020.
+//
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <io/dimacs_reader.h>
+
+TEST_SUITE("IO library") {
+
+	TEST_CASE("Read DIMACS file") {
+
+	}
+
+}
