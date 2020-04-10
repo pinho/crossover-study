02df8b17fde2cec06079360dccbf74995b55bc17
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-04-10 19:14:21 -0300

    Remove doctest from into the project

--- ccr/test/test_io.cpp
+++ ccr/test/test_io.cpp
@@ -2,8 +2,8 @@
 // Created by ronaldd on 09/04/2020.
 //
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include <io/dimacs_reader.h>
+#include <doctest/doctest.h>
+#include <ccr/io/dimacs_reader.h>
 
 TEST_SUITE("IO library") {
 
