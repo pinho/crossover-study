924ba32551ea2d328d1456a508f7a89b98b32e6f
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-04-12 15:50:47 -0300

    Remove arg.hxx and create CLI using getopt.h

--- ccr/src/argparser.h
+++ ccr/src/argparser.h
@@ -2,36 +2,71 @@
 #define CCR_ARG_PARSER_H
 
 #include <iostream>
-#include "args.hxx"
-#include "settings.h"
 
-namespace CLI {
+// namespace CLI {
+//     // Definção das opções de linha de comandos
+//     args::ArgumentParser parser(PROGRAM_RUNNER);
+//     args::ValueFlag<std::string> config(parser, "file",
+//             "Arquivo de configuração de execução", {'c', "config"});
+//     // Parse options passed in command-line options
+//     void Parse(int argc, char **argv) {
+//         try {
+//             parser.ParseCLI(argc, argv);
+//         } catch (args::Help&) {
+//             std::cout << parser.Help();
+//             exit(0);
+//         } catch (args::Error &e) {
+//             std::cerr << e.what() << std::endl;
+//             exit(2);
+//         }
+//     }
+//     // Returns the string getted from command-line option for configuration
+//     // file, or the default value for it
+//     std::string ConfigFileName() {
+//         return bool(config) ? args::get(config) : "config.yml";
+//     }
+// }
 
-    // Definção das opções de linha de comandos
-    args::ArgumentParser parser(PROGRAM_RUNNER);
-    args::ValueFlag<std::string> config(parser, "file",
-            "Arquivo de configuração de execução", {'c', "config"});
+#include <getopt.h>
+#include <string>
 
-    // Parse options passed in command-line options
-    void Parse(int argc, char **argv) {
-        try {
-            parser.ParseCLI(argc, argv);
-        } catch (args::Help&) {
-            std::cout << parser.Help();
-            exit(0);
-        } catch (args::Error &e) {
-            std::cerr << e.what() << std::endl;
-            exit(2);
+
+namespace cli {
+
+  const char *SHORT_OPTIONS = "c:h";
+
+  const option LONG_OPTIONS[] = {
+    {"config", required_argument, nullptr, 'c'},
+    {"help", no_argument, nullptr, 'h'},
+    {nullptr, no_argument, nullptr, 0}
+  };
+
+  std::string configfilename = "config.yml";
+
+
+  void print_help() {
+      std::cout << "Options:\n\n";
+      for (int i = 0; i < 2; i++) {
+          std::cout << " -" << char(LONG_OPTIONS[i].val) << ", --" << LONG_OPTIONS[i].name << "\n";
+      }
+  }
+
+
+  void parse(int argc, char **argv) {
+    int opt;
+    while ((opt = getopt_long(argc, argv, SHORT_OPTIONS, LONG_OPTIONS, 0)) != EOF) {
+        switch (opt) {
+            case 'c':
+                if (optarg) configfilename = std::string(optarg);
+                break;
+            case 'h':
+            default:
+                print_help();
+                break;
         }
     }
+  }
 
-    // Returns the string getted from command-line option for configuration
-    // file, or the default value for it
-    std::string ConfigFileName() {
-        return bool(config) ? args::get(config) : "config.yml";
-    }
-
-    
 
 }
 
--- ccr/src/main.cpp
+++ ccr/src/main.cpp
@@ -7,22 +7,7 @@ using std::endl;
 
 
 int main(int argc, char **argv) {
-	CLI::Parse(argc, argv);
-
-	try {
-		auto conf = Configuration(CLI::ConfigFileName());
-
-		cout << conf << endl;
-
-
-	} catch (YAML::BadFile &bf) {
-		std::cerr << bf.what() << std::endl;
-		std::cout << "Arquivo inválido -- config.yml não encontrado." << std::endl;
-		return 1;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-		return 1;
-	}
+	cli::parse(argc, argv);
 
 	return 0;
 }
