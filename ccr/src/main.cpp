ebabeda0794bc8dc146f9a6869c3ceb4f1d31918
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-04-23 19:43:59 -0300

    Update main.cpp

--- ccr/src/main.cpp
+++ ccr/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <ccr/config/configuration.h>
 #include "argparser.h"
+
+#include <ccr/ga/crossover_fabric.h>
 
 using std::cout;
 using std::endl;
@@ -10,6 +11,14 @@ int main(int argc, char **argv) {
 	cli::parse(argc, argv);
 
 	std::cout << "Arquivo de configuração: " << cli::configfilename << std::endl;
+
+	eoQuadOp<Chrom>* x1_ptr = CrossoverFabric::create_uniform();
+	auto x2_ptr = CrossoverFabric::create_cutpoints(2);
+	auto x3_ptr = CrossoverFabric::create(0);
+
+	std::cout << x1_ptr->className() << std::endl;
+	std::cout << x2_ptr->className() << std::endl;
+	std::cout << x3_ptr->className() << std::endl;
 
 	return 0;
 }
