fcaa265f37d68b44558a0dc6cd152eb32b7e3619
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-04-28 14:28:02 -0300

    Add function to init population in Random class

--- include/ga/random.cpp
+++ include/ga/random.cpp
@@ -1,10 +1,17 @@
-#include "encoding.h"
 #include "random.h"
+
+// Gerador de valores booleanos aleatórios (usado nas implementações de Random)
+eoBooleanGenerator BOOLRNG;
 
 template <>
 Chrom Random<Chrom>::uniform_chromosome (uint csize) {
-    eoBooleanGenerator _gen;
-    eoInitFixedLength<Chrom> _init(csize, _gen);
+    eoInitFixedLength<Chrom> _init(csize, BOOLRNG);
     Chrom x; _init(x);
     return x;
+}
+
+template <>
+eoPop<Chrom> Random<Chrom>::population(uint chromsize, uint popsize) {
+    eoInitFixedLength<Chrom> _init(chromsize, BOOLRNG);
+    return eoPop<Chrom>(popsize, _init);
 }
--- include/ga/random.h
+++ include/ga/random.h
@@ -1,10 +1,20 @@
 #ifndef CROSSOVERRESEARCH_RANDOM_H
 #define CROSSOVERRESEARCH_RANDOM_H
 
+#include <paradiseo/eo/utils/eoRndGenerators.h>
+#include <paradiseo/eo/eoPop.h>
+#include "encoding.h"
 
 template <class Encoding> class Random {
 public:
-    static Encoding uniform_chromosome(uint);
+    /**
+     * Function used to generate a chromosome instance
+     * using uniform distribution */
+    static Encoding uniform_chromosome(uint chromsize);
+
+    /**
+     * Function used to initialize a population of chromosomes randomly */
+    static eoPop<Encoding> population(uint chromsize, uint popsize);
 };
 
 #endif//CROSSOVERRESEARCH_RANDOM_H
