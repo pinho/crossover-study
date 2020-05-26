328495eca801cf7b0641bf06b22064d7aa80f33e
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-05-26 11:22:17 -0300

    Novo script para re compilar

---
+++ reDeloy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Apagar a pasta build se existir
+rm -rf build
+
+# recriar a pasta build
+mkdir build 
+cd build
+
+# Gerar arquivos de bild com cmake
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
+
+# Compilar e instalar
+cmake --build . --config Release --target install
+
+cd ..
+
