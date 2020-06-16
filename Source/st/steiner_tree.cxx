46cef3f825d6d662962ead6480c7ae284f8e9090
Ronaldd Pinho <ronaldppinho@gmail.com>
2020-06-16 14:10:09 -0300

    Limpando estruturas

--- Source/st/steiner_tree.cxx
+++
@@ -1,9 +0,0 @@
-#include "../macros.h"
-#include "../problems.hxx"
-
-#include <iostream>
-using namespace std;
-
-void problem::steiner_tree(int argc, char **argv) {
-  PRINT_RED_BG("Árvore de Steiner");
-}
